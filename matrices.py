376a3d7293568ec84b5f7f50a890672c0897b997
Henrique Varnier Bridi <henriquevarnierbridi@gmail.com>
2026-03-24 22:10:55 +0100

    split the checking matrix function into the a function to check if the matrix is valid and another one to check if it has a rectangular shape to improve reusability :)

--- matrices.py
+++ matrices.py
@@ -1,32 +1,37 @@
-def check_shape(matrix1 : list[list[int]], matrix2 : list[list[int]]):
-    if not isinstance(matrix1, list) or not isinstance(matrix2, list) or len(matrix1) == 0 or len(matrix2) == 0:
+def check_if_matrix(matrix1 : list[list[int]]):
+    if not isinstance(matrix1, list) or len(matrix1) == 0:
         print('Please insert a non-empty and valid matrix')
-        return
-    for row in (matrix1 + matrix2):
+        return False
+    for row in (matrix1):
         if not isinstance(row, list):
             print('Please insert a valid matrix')
-            return
+            return False
     if len(matrix1[0]) == 0:
         print('Please insert a non-empty matrix')
-        return
+        return False
+    return True
+    
+def check_same_shape(matrix1, matrix2):
+    if not check_if_matrix(matrix1) or not check_if_matrix(matrix2):
+        return False
     n_rows = len(matrix1)
     n_columns = len(matrix1[0])
     for row in matrix1:
         if len(row) != n_columns:
             print('Not a valid matrix')
-            return
+            return False
     if len(matrix2) != n_rows:
         print('The matrices need to have the same shape')
-        return
+        return False
     for row in matrix2:
         if len(row) != n_columns:
             print('The matrices need to have the same shape')
-            return
+            return False
     return True
 
         
 def sum_matrix(matrix1, matrix2):
-    if not check_shape(matrix1, matrix2):
+    if not check_same_shape(matrix1, matrix2):
         return
     matrix3 = []
     for i_row, row in enumerate(matrix1):
@@ -37,7 +42,7 @@ def sum_matrix(matrix1, matrix2):
     return matrix3
 
 def sub_matrix(matrix1, matrix2):
-    if not check_shape(matrix1, matrix2):
+    if not check_same_shape(matrix1, matrix2):
         return
     matrix3 = []
     for i_row, row in enumerate(matrix1):
